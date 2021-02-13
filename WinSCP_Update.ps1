d9c3a35564d69ff1674d9921d62574caa182e659
MalaGaM <malagam.artispretis@gmail.com>
2021-02-13 18:49:35 +0100

    Conf : lang, path and verbose

--- WinSCP_Update.ps1
+++ WinSCP_Update.ps1
@@ -1,31 +1,64 @@
+###########################
+# HOMEPAGE : https://github.com/MalaGaM/POWERSHELL-WinSCP-PORTABLE-FR-UPDATE
+# AUTHOR   : malagam.artispretis@gmail.com
+#
+# Update your WinSCP portable with your lang
+#
+###########################
 
-$URL_HOME_DOWNLOAD_PAGE = 'https://winscp.net/eng/downloads.php'
-$REGEXP_FILE1  = 'WinSCP.+Portable\.zip$'
-$REGEXP_FILE2  = 'https://winscp.net/download/files/'
-$HomePageDownload = Invoke-WebRequest -Uri $URL_HOME_DOWNLOAD_PAGE -UseBasicParsing
-$url = @($HomePageDownload.links | ? href -match $REGEXP_FILE1) -notmatch 'beta|rc' | % href
-$url = 'https://winscp.net/eng' + $url
-$version   = $url -split '-' | select -Last 1 -Skip 1
-$file_name = $url -split '/' | select -last 1
-$FILEGET		= "https://winscp.net/download/${file_name}"
-$PageDownload = Invoke-WebRequest -Uri $FILEGET -UseBasicParsing
-$DIRECT_DOWNLOAD = @($PageDownload.links | ? href -match $REGEXP_FILE2) | % href
-echo "Download URL: $DIRECT_DOWNLOAD"
+####################
+# CONFIGURATION:
+$CONF_LANG		=	"fr"
+$INSTALL_DIR	=	"D:\_APP_PORTABLE\WinSCP"
+$VERBOSE		=	1
+####################
+
+####################
+# CODE:
+function RMFILE {
+	param (
+		$FILE_NAME
+	)
+	if (Test-Path $FILE_NAME) {
+		Remove-Item $FILE_NAME
+		if ($VERBOSE -eq 1) { echo "File removed: $FILE_NAME" }
+	}
+}
+## VARS
+$URL_WEBSITE				= "https://winscp.net"
+$URL_HOME_DOWNLOAD_PAGE		= "${URL_WEBSITE}/eng/downloads.php"
+$URL_LANG_DOWNLOAD_PAGE		= "${URL_WEBSITE}/eng/translations.php"
+
+$RE_GET_WinSCP_Filename		= "WinSCP.+Portable\.zip$"
+$RE_GET_WinSCP_URL			= "${URL_WEBSITE}/download/files/"
+## DOWNLOAD WinSCP
+$HomePageDownload			= Invoke-WebRequest -Uri $URL_HOME_DOWNLOAD_PAGE -UseBasicParsing
+$url						= @($HomePageDownload.links | ? href -match ${RE_GET_WinSCP_Filename}) -notmatch "beta|rc" | % href
+$url						= "${URL_WEBSITE}/eng" + $url
+$version					= $url -split "-" | select -Last 1 -Skip 1
+$file_name					= $url -split "/" | select -last 1
+$FILEGET					= "${URL_WEBSITE}/download/${file_name}"
+$PageDownload				= Invoke-WebRequest -Uri $FILEGET -UseBasicParsing
+$DIRECT_DOWNLOAD			= @($PageDownload.links | ? href -match ${RE_GET_WinSCP_URL}) | % href
+if ($VERBOSE -eq 1) { echo "Download URL: $DIRECT_DOWNLOAD" }
 Invoke-WebRequest -Uri "$DIRECT_DOWNLOAD" -OutFile "$file_name"
-Expand-Archive -Path $file_name -DestinationPath . -Force
-Remove-Item $file_name
-# LANG FR
-$URL_LANG_DOWNLOAD_PAGE = 'https://winscp.net/eng/translations.php'
-$REGEXP_FILELANG  = "${version}/fr\.zip$"
-$LangPageDownload = Invoke-WebRequest -Uri $URL_LANG_DOWNLOAD_PAGE -UseBasicParsing
-$LANG_DIRECT_LINK = @($LangPageDownload.links | ? href -match $REGEXP_FILELANG) | % href
-$LANG_DIRECT_DOWNLOAD = "https://winscp.net/eng/${LANG_DIRECT_LINK}"
-echo "Download URL: $LANG_DIRECT_DOWNLOAD"
-Invoke-WebRequest -Uri "$LANG_DIRECT_DOWNLOAD" -OutFile "fr.zip"
+if ($VERBOSE -eq 1) { echo "Extract files from $file_name" }
+Expand-Archive -Path $file_name -DestinationPath ${INSTALL_DIR} -Force
+RMFILE $file_name
 
-Expand-Archive -Path fr.zip -DestinationPath . -Force
-Remove-Item fr.zip
-Remove-Item license.txt
-Remove-Item debug.log
-Remove-Item readme.txt
+## DOWNLOAD WinSCP Lang
+$LangPageDownload			= Invoke-WebRequest -Uri $URL_LANG_DOWNLOAD_PAGE -UseBasicParsing
+$RE_GET_WinSCP_LANG_URL		= "${version}/${CONF_LANG}\.zip$"
+$LANG_DIRECT_LINK			= @($LangPageDownload.links | ? href -match ${RE_GET_WinSCP_LANG_URL}) | % href
+$LANG_DIRECT_DOWNLOAD		= "${URL_WEBSITE}/eng/${LANG_DIRECT_LINK}"
+if ($VERBOSE -eq 1) { echo "Download URL: $LANG_DIRECT_DOWNLOAD" }
+Invoke-WebRequest -Uri "$LANG_DIRECT_DOWNLOAD" -OutFile "${CONF_LANG}.zip"
+if ($VERBOSE -eq 1) { echo "Extract files from ${CONF_LANG}.zip" }
+Expand-Archive -Path "${CONF_LANG}.zip" -DestinationPath ${INSTALL_DIR} -Force
+RMFILE "${CONF_LANG}.zip"
+RMFILE "${INSTALL_DIR}/license.txt"
+RMFILE "${INSTALL_DIR}/debug.log"
+RMFILE "${INSTALL_DIR}/readme.txt"
+Start-Process -FilePath "${INSTALL_DIR}/WinSCP.exe"
+####################
 
